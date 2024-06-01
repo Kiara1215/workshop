f5f7d245fb816307c4218a81828c45fda9713f20
zhangle39 <zhangle39@meituan.com>
2024-06-01 17:11:43 +0800

    1493

--- src/main/java/data_structure_algorithm/leetcode/_1493longestSubarray.java
+++ src/main/java/data_structure_algorithm/leetcode/_1493longestSubarray.java
@@ -2,6 +2,25 @@ package data_structure_algorithm.leetcode;
 
 public class _1493longestSubarray {
 
+    public static class Solution0 {
+
+        /**
+         思路：判断滑动窗口里0的个数，来调整左右指针。记录长度的时候，需要减掉1个元素
+         */
+        public int longestSubarray(int[] nums) {
+            int n = nums.length, left = 0, right = 0, maxLen = 0, count0 = 0;
+            while(right < n){
+                if(nums[right++] == 0) count0++;
+                while(count0>1){
+                    if(nums[left++]==0) count0--;
+                }
+                maxLen = Math.max(maxLen, right-left-1);
+            }
+            return maxLen;
+        }
+
+    }
+
     public static class Solution1 {
 
         /**
